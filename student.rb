3b2220432ee2f7069395f92920444680d7f5576c
Trust7 <simeontrust7@gmail.com>
2023-02-01 14:11:15 +0100

    Implemented associations in classes

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require './person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(id, age, classroom, name = 'unknown', parent_permission: true)
     super(id, name, age, parent_permission)
     @classroom = classroom
@@ -8,5 +10,10 @@ class Student < Person
 
   def play_hooky
     puts '¯(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 end
